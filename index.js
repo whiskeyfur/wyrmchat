a6799d05b8e84b41f3c3b3a17d0f324e7d14d8fc
whiskeyfur <john.walker.ii@gmail.com>
2022-03-15 17:01:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -21,11 +21,15 @@ wss.on("connection", function(ws) {
   }, 1000)
    
   ws.on('message', function message(data, isBinary) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
-      }
-    });
+    try {
+      wss.clients.forEach(function each(client) {
+       if (client.readyState === WebSocket.OPEN) {
+          client.send(data, { binary: isBinary });
+        }
+      });
+    } catch(ex) {
+      console.log(JSON.stringify(ex));
+    }
   });
 
   ws.on("close", function() {
